9565a4015a753b81b2b48818fbf7d71a98fa9df5
Rohan Balakrishnan <rohanbk@gmail.com>
2019-01-06 07:25:49 -0800

    feat(all): First commit

---
+++ cards/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for _, card := range cards {
+		fmt.Println(card)
+	}
+}
+
+func newCard() string {
+	return "5 of diamonds"
+}
