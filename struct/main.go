10f9bedd0b99a228a99e33869a85a65ee9ac31dd
Rohan Balakrishnan <rohanbk@gmail.com>
2019-01-14 19:11:45 -0800

    feat(struct): Example struct program

---
+++ struct/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+
+	alexContact := contactInfo{
+		email:   "alex@gmail.com",
+		zipCode: 94000,
+	}
+
+	alex := person{
+		firstName:   "Alex",
+		lastName:    "Al",
+		contactInfo: alexContact,
+	}
+	alex.print()
+
+	alex.updateName("Alexander", "Allen-Benedict")
+	alex.print()
+
+}
+
+func (pointerToPerson *person) updateName(firstName string, lastName string) {
+	//Turn memory address into a value using '*'
+	pointerToPerson.firstName = firstName
+	pointerToPerson.lastName = lastName
+
+}
+
+func (pointerToPerson *person) print() {
+	fmt.Printf("%+v\n", *pointerToPerson)
+}
